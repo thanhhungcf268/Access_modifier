1aca55ace74aab35521096f514004e4890179527
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-04-08 16:04:42 +0700

    3. Protected

---
+++ myPack/B.java
@@ -0,0 +1,17 @@
+package mypack;
+
+import pack.*;
+
+
+
+class B extends A{
+
+    public static void main(String args[]){
+
+        B obj = new B();
+
+        obj.msg();
+
+    }
+
+}
---
+++ mypack1/B.java
@@ -0,0 +1,17 @@
+package mypack1;
+
+import pack1.*;
+
+
+
+class B extends A{
+
+    public static void main(String args[]){
+
+        B obj = new B();
+
+        obj.msg();
+
+    }
+
+}
