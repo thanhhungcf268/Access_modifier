f9e7975c36934472d6aceeeaeaa21c147033df82
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-04-08 16:00:56 +0700

    package

---
+++ myPack/mypack.java
@@ -0,0 +1,15 @@
+package mypack;
+
+import pack.*;
+
+class B{
+
+    public static void main(String args[]){
+
+        A obj = new A();//Compile Time Error
+
+        obj.msg();//Compile Time Error
+
+    }
+
+}
